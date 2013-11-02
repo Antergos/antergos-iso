2c2b73c6fbbd1ae4ce0c0fe7cbf6ccdc33a38ba7
Alexandre Filgueira <faidoc@gmail.com>
2013-11-02 15:50:02 -0500

    Fixed syslinux

--- configs/antergos/build.sh
+++ configs/antergos/build.sh
@@ -62,9 +62,7 @@ make_boot_extra() {
 make_isolinux() {
     if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
         cp -Lr isolinux ${work_dir}/iso
-        cp ${work_dir}/root-image/usr/lib/syslinux/bios/isolinux.bin ${work_dir}/iso/isolinux/
-        cp ${work_dir}/root-image/usr/lib/syslinux/bios/isohdpfx.bin ${work_dir}/iso/isolinux/
-        cp ${work_dir}/root-image/usr/lib/syslinux/bios/ldlinux.c32 ${work_dir}/iso/isolinux
+        cp -R ${work_dir}/root-image/usr/lib/syslinux/bios/* ${work_dir}/iso/isolinux/
         sed "s|%ARCHISO_LABEL%|${iso_label}|g;
                 s|%INSTALL_DIR%|${install_dir}|g;
                 s|%ARCH%|${arch}|g" ${script_path}/isolinux/isolinux.cfg > ${work_dir}/iso/isolinux/isolinux.cfg
