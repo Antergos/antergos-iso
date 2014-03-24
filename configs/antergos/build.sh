489971458ed3ce971d1cae22c61288a27479495f
Alex Filgueira <faidoc@gmail.com>
2014-03-24 16:22:37 -0500

    I thought I had commited the blank password for antergos user long time ago

--- configs/antergos/build.sh
+++ configs/antergos/build.sh
@@ -163,7 +163,7 @@ make_customize_root_image() {
             run
 
         mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
-            -r 'useradd -m -g users -G "audio,disk,optical,wheel,network,autologin" antergos' \
+            -r 'useradd -p U6aMy0wojraho -m -g users -G "audio,disk,optical,wheel,network,autologin" antergos' \
             run
 
         # Configuring pacman
