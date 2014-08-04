7275e65cd4260a0cecc0c4cc9ddc3fd59757ba92
Dustin Falgout <dustin@falgout.us>
2014-08-04 09:08:14 -0500

    typo fix again

--- configs/antergos/build.sh
+++ configs/antergos/build.sh
@@ -205,7 +205,7 @@ make_customize_root_image() {
         	cp ${script_path}/set_password ${work_dir}/root-image/usr/bin
         	chmod +x ${work_dir}/root-image/usr/bin/set_password
         	mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
-           	-r '/usr/bin/set_passwd' \
+           	-r '/usr/bin/set_password' \
            	run
            	rm ${work_dir}/root-image/usr/bin/set_password
 		#echo "antergos:U6aMy0wojraho" | chpasswd -R /antergos-iso/configs/antergos/${work_dir}/root-image
