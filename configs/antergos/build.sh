c56957c1636d59684105d41ddc67e250cbef5ddd
Dustin Falgout <dustin@falgout.us>
2015-08-10 01:00:38 -0500

    trying to fix xorg crash under vbox

--- configs/antergos/build.sh
+++ configs/antergos/build.sh
@@ -276,8 +276,9 @@ make_customize_root_image() {
         part_five() {
         
         	mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
-            	-r 'systemctl -fq enable pacman-init lightdm NetworkManager ModemManager livecd vboxservice NetworkManager-wait-online' \
+            	-r 'systemctl -fq enable dkms pacman-init lightdm NetworkManager ModemManager livecd vboxservice NetworkManager-wait-online' \
             	run
+            	
             mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
             	-r 'systemctl -fq disable pamac' \
             	run
