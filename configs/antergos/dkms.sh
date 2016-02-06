e27cc2fdbea6d6f8d9abc877f6b28efbd4322934
Dustin Falgout <dustin@falgout.us>
2016-02-06 11:36:56 -0600

    good job genious :stuck_out_tongue_winking_eye:

--- configs/antergos/dkms.sh
+++ configs/antergos/dkms.sh
@@ -3,14 +3,13 @@
 _KERNVER_STR="$(pacman -Q linux)"
 _KERNVER="${_KERNVER_STR/linux }"
 
-check_initramfs() {
-	echo '>>> Updating module dependencies. Please wait ...'
-	
-	dkms add -k "${_KERNVER}" "zfs/${_PKGVER}"
-	dkms install -k "${_KERNVER}" "zfs/${_PKGVER}"
-	modprobe -a zfs
 
-	echo '>>> Generating initial ramdisk, using mkinitcpio. Please wait...'
+echo '>>> Updating module dependencies. Please wait ...'
 
-	mkinitcpio -p linux
-}
+dkms add -k "${_KERNVER}" "zfs/${_PKGVER}"
+dkms install -k "${_KERNVER}" "zfs/${_PKGVER}"
+modprobe -a zfs
+
+echo '>>> Generating initial ramdisk, using mkinitcpio. Please wait...'
+
+mkinitcpio -p linux
