2c538068f3b7f8089f41a4cb58bc711402671cfc
Dustin Falgout <dustin@falgout.us>
2016-02-06 21:42:51 -0600

    ...

--- configs/antergos/dkms.sh
+++ configs/antergos/dkms.sh
@@ -5,7 +5,12 @@ _KERNVER="${_KERNVER_STR/linux }-ARCH"
 
 echo '>>> Updating module dependencies. Please wait ...'
 
-{ dkms install -k "${_KERNVER}" "spl/0.6.5.4" && dkms install -k "${_KERNVER}" "zfs/0.6.5.4"; } || exit 1
+if [[ $(dkms status -k "${_KERNVER}" spl/0.6.5.4) != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "spl/0.6.5.4"; } || exit 1
+fi
 
-{ modprobe -a spl zfs && mkinitcpio -p linux && exit 0; } || exit 1
+if [[ $(dkms status -k "${_KERNVER}" zfs/0.6.5.4) != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "zfs/0.6.5.4"; } || exit 1
+fi
 
+{ mkinitcpio -p linux && exit 0; } || exit 1
