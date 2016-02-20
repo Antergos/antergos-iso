3bb91909afb4f8451f91473afbb7525782a5dc17
Dustin Falgout <dustin@falgout.us>
2016-02-20 10:40:07 -0600

    ...

--- configs/antergos/build.sh
+++ configs/antergos/build.sh
@@ -278,13 +278,6 @@ make_customize_root_image() {
         }
         
         part_five() {
-        	mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
-            	-r 'pacman -R --noconfirm syslinux' \
-            	run
-            
-            mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
-            	-r 'pacman -Syy --noconfirm syslinux-tmp' \
-            	run
         
         	mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
             	-r 'systemctl -fq enable pacman-init lightdm-plymouth plymouth-start NetworkManager ModemManager livecd vboxservice NetworkManager-wait-online' \
