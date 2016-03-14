9438450ccf940189f651fe2f08ea43d6a52e4c2f
Dustin Falgout <dustin@falgout.us>
2016-03-14 08:39:18 -0500

    prep for iso build

--- configs/antergos/dkms.sh
+++ configs/antergos/dkms.sh
@@ -5,12 +5,16 @@ _KERNVER="${_KERNVER_STR/linux }-ARCH"
 
 echo '>>> Updating module dependencies. Please wait ...'
 
-if [[ $(dkms status -k "${_KERNVER}" spl/0.6.5.4) != *'installed'* ]]; then
-	{ dkms install -k "${_KERNVER}" "spl/0.6.5.4"; } || exit 1
+if [[ $(dkms status -k "${_KERNVER}" spl/0.6.5.5) != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "spl/0.6.5.5"; } || exit 1
 fi
 
-if [[ $(dkms status -k "${_KERNVER}" zfs/0.6.5.4) != *'installed'* ]]; then
-	{ dkms install -k "${_KERNVER}" "zfs/0.6.5.4"; } || exit 1
+if [[ $(dkms status -k "${_KERNVER}" zfs/0.6.5.5) != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "zfs/0.6.5.5"; } || exit 1
+fi
+
+if [[ $(dkms status -k "${_KERNVER}" vboxguest/5.0.16_OSE) != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "vboxguest/5.0.16_OSE"; } || exit 1
 fi
 
 { mkinitcpio -p linux && exit 0; } || exit 1
