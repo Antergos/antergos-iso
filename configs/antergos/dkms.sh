b7e808cabf09eae527d69987738ff1096b18b38c
Dustin Falgout <dustin@falgout.us>
2016-04-16 05:25:37 -0500

    fix dkms.sh

--- configs/antergos/dkms.sh
+++ configs/antergos/dkms.sh
@@ -2,15 +2,18 @@
 
 _KERNVER_STR="$(pacman -Q linux)"
 _KERNVER="${_KERNVER_STR/linux }-ARCH"
+_MODVER_STR="$(pacman -Q zfs)"
+_MODVER_STR="${_MODVER_STR/zfs }"
+_MODVER="${_MODVER_STR/.r*}"
 
 echo '>>> Updating module dependencies. Please wait ...'
 
-if [[ $(dkms status -k "${_KERNVER}" spl/0.6.5.5) != *'installed'* ]]; then
-	{ dkms install -k "${_KERNVER}" "spl/0.6.5.5"; } || exit 1
+if [[ $(dkms status -k "${_KERNVER}" "spl/${_MODVER}") != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "spl/${_MODVER}"; } || exit 1
 fi
 
-if [[ $(dkms status -k "${_KERNVER}" zfs/0.6.5.5) != *'installed'* ]]; then
-	{ dkms install -k "${_KERNVER}" "zfs/0.6.5.5"; } || exit 1
+if [[ $(dkms status -k "${_KERNVER}" "zfs/${_MODVER}") != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "zfs/${_MODVER}"; } || exit 1
 fi
 
 if [[ $(dkms status -k "${_KERNVER}" vboxguest/5.0.16_OSE) != *'installed'* ]]; then
