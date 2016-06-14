c73404abc41b815a3c73efb3154f2ebfd91bd288
Dustin Falgout <dustin@falgout.us>
2016-06-14 10:55:30 -0500

    fix issue with downoloadeing updater script

--- configs/openbox/build.sh
+++ configs/openbox/build.sh
@@ -363,6 +363,11 @@ make_customize_root_image() {
 			 mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
             	-r 'pacman -Scc --noconfirm' \
             	run
+            
+             # GDK Pixbuf Bug
+        	mkarchiso ${verbose} -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" \
+            	-r 'gdk-pixbuf-query-loaders --update-cache' \
+            	run
 		
 			# Install translations for updater script
         	( "${script_path}/translations.sh" $(cd "${out_dir}"; pwd;) $(cd "${work_dir}"; pwd;) $(cd "${script_path}"; pwd;) )
