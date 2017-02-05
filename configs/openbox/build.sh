f0d6b1e941b73fe9919978e3aa3e88630baa54c9
Dustin Falgout <dustin@falgout.us>
2017-02-05 01:30:55 -0600

    mirrorlist and pacman.conf changes from default iso

--- configs/openbox/build.sh
+++ configs/openbox/build.sh
@@ -22,8 +22,7 @@ setup_workdir() {
     cache_dirs="/var/cache/pacman/pkg"
     mkdir -p "${work_dir}"
     pacman_conf="${work_dir}/pacman.conf"
-    sed -r "s|^#?\\s*CacheDir.+|CacheDir = $(echo -n ${cache_dirs[@]})|g" \
-        "${script_path}/pacman.conf.${arch}" > "${pacman_conf}"
+    sed -r "s|^#?\\s*CacheDir.+|CacheDir = $(echo -n ${cache_dirs})|g" "${script_path}/pacman.conf" > "${pacman_conf}"
 }
 
 # Base installation (root-image)
@@ -262,9 +261,9 @@ make_customize_root_image() {
         	ln -sf /usr/share/zoneinfo/UTC ${work_dir}/root-image/etc/localtime
         	chmod 750 ${work_dir}/root-image/etc/sudoers.d
         	chmod 440 ${work_dir}/root-image/etc/sudoers.d/g_wheel
-        	mkdir -p ${work_dir}/root-image/etc/pacman.d
-        	wget -O ${work_dir}/root-image/etc/pacman.d/mirrorlist 'https://www.archlinux.org/mirrorlist/?country=all&protocol=http&use_mirror_status=on'
-        	sed -i "s/#Server/Server/g" ${work_dir}/root-image/etc/pacman.d/mirrorlist
+        	#mkdir -p ${work_dir}/root-image/etc/pacman.d
+        	#wget -O ${work_dir}/root-image/etc/pacman.d/mirrorlist 'https://www.archlinux.org/mirrorlist/?country=all&protocol=http&use_mirror_status=on'
+        	#sed -i "s/#Server/Server/g" ${work_dir}/root-image/etc/pacman.d/mirrorlist
         	#mkdir -p ${work_dir}/root-image/var/run/dbus
         	#mount -o bind /var/run/dbus ${work_dir}/root-image/var/run/dbus
         	# Download opendesktop-fonts
@@ -310,7 +309,7 @@ make_customize_root_image() {
 		#echo "antergos:U6aMy0wojraho" | chpasswd -R /antergos-iso/configs/antergos/${work_dir}/root-image
         	# Configuring pacman
 		echo "Configuring Pacman"
-        	cp -f ${script_path}/pacman.conf.x86_64 ${work_dir}/root-image/etc/pacman.conf
+        	cp -f ${script_path}/pacman.conf ${work_dir}/root-image/etc/pacman.conf
         	sed -i 's|^#CheckSpace|CheckSpace|g' ${work_dir}/root-image/etc/pacman.conf
         	sed -i 's|^#SigLevel = Optional TrustedOnly|SigLevel = Optional|g' ${work_dir}/root-image/etc/pacman.conf
         	if [[ ${arch} == 'x86_64' ]]; then
