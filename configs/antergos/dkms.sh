1f13d65ce509c5435c45dcf7319d4999992551a0
karasu <karasu@antergos.com>
2017-02-05 13:14:46 +0100

    dkms not found

--- configs/antergos/dkms.sh
+++ configs/antergos/dkms.sh
@@ -6,7 +6,7 @@ _MODVER_STR="$(pacman -Q zfs)"
 _MODVER_STR="${_MODVER_STR/zfs }"
 _MODVER="${_MODVER_STR/.r*}"
 
-pacman -S --noconfirm --needed linux-headers
+pacman -S --noconfirm --needed linux-headers dkms
 
 echo '>>> Updating module dependencies. Please wait ...'
 
