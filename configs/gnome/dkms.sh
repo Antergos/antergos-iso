a0f892c6eeb51156a325e88390eb78a15cb5c861
karasu <gustaucastells@gmail.com>
2018-08-19 19:13:24 +0200

    Update dkms.sh from master

--- configs/gnome/dkms.sh
+++ configs/gnome/dkms.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 _KERNVER_STR="$(pacman -Q linux)"
-_KERNVER="${_KERNVER_STR/linux }-ARCH"
+_KERNVER="${_KERNVER_STR/linux }"
+_KERNVER="${_KERNVER%-*}"
+_KERNVER="${_KERNVER/.arch1/-arch1-1-ARCH}"
 _MODVER_STR="$(pacman -Q zfs)"
 _MODVER_STR="${_MODVER_STR/zfs }"
-_MODVER="${_MODVER_STR/.r*}"
+_MODVER="${_MODVER_STR%-*}"
 
-# I'm doing something wrong, all this should be already created/installed
-reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-haveged -w 1024
-pacman-key --init
-pacman-key --populate archlinux antergos
-pacman -S --noconfirm --needed linux-headers dkms
+pacman -S --needed linux-headers
+
+# :unamused:
+#[[ -e "/usr/lib/modules/${_KERNVER}" ]] || _KERNVER="${_KERNVER,,}"
+#[[ -e "/usr/lib/modules/${_KERNVER}" ]] || _KERNVER="${_KERNVER}1"
+
 
 echo '>>> Updating module dependencies. Please wait ...'
 
@@ -23,4 +25,4 @@ if [[ $(dkms status -k "${_KERNVER}" "zfs/${_MODVER}") != *'installed'* ]]; then
 	{ dkms install -k "${_KERNVER}" "zfs/${_MODVER}"; } || exit 1
 fi
 
-{ mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz-linux -g /boot/archiso.img && exit 0; } || exit 1
+{ mkinitcpio -p linux && exit 0; } || exit 1
